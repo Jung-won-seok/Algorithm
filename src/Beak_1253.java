7b0ca2052262548dfe1b0433a0ef81612c4b32b0
Wonseok Jung <ukksj0621@naver.com>
2023-08-17 12:51:14 +0900

    commit

--- src/Beak_1253.java
+++ src/Beak_1253.java
@@ -8,5 +8,28 @@ public class Beak_1253 {
         for(int i=0;i<N;i++){
             A[i]=sc.nextInt();
         }
+        int sum=0; int cnt=0;
+        for(int i=0;i<=N-1;i++){
+            int a = i;
+            int b = i+1;
+
+            while(b<N-1){
+                sum = A[a] + A[b];{
+                    while(b<N-1){
+                        int c = b+1;
+                        if(sum == A[c]){
+                            cnt++; break;
+                        }
+                        else if(sum >= A[c]){
+                            c++;
+                            continue;
+                        }
+                        b++;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
     }
 }
--- src/Beak_12891.java
+++ src/Beak_12891.java
@@ -1,2 +1,7 @@
+import java.io.*;
+import java.util.Scanner;
 public class Beak_12891 {
+    public static void main(String[] args){
+        
+    }
 }
