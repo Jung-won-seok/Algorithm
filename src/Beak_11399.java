234bfec930ad8bd03be96f0671e532c0e428ca27
Wonseok Jung <ukksj0621@naver.com>
2023-09-10 17:03:54 +0900

    commit

--- src/Beak_11399.java
+++ src/Beak_11399.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 public class Beak_11399 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int N = sc.nextInt(); //사람 수
         int temp = 0;
-        int Person[] = new int[N];
+        int Person[] = new int[N]; //사람 줄
         for (int i = 0; i < N; i++) {
             Person[i] = sc.nextInt();
         }
-        for (int i = 1; i < N; i++) { //최솟값을 위한 오름차순 배열
+        for (int i = 1; i < N; i++) { //최솟값 배열을 위한 오름차순 배열
             for (int j = i - 1; j >= 0; j--) {
-                if (Person[i] >= Person[j]) {
+                if (Person[i] <= Person[j]) {
                     temp = Person[i];
-                    for (int k = j + 1; k < i; k--) {
+                    for (int k = i-1 ; k>=j ; k--) {
                         Person[k + 1] = Person[k];
                     }
-                    Person[j + 1] = temp;
+                    Person[j] = temp;
                 }
             }
         }
