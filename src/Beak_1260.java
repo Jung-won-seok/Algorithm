3fc59778092e9c20e03f9c7461d616df6d83e536
Wonseok Jung <ukksj0621@naver.com>
2023-10-05 15:07:57 +0900

    commit

--- src/Beak_1260.java
+++ src/Beak_1260.java
@@ -2,13 +2,16 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Beak_1260 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         int A[] = new int[n];
-        for(int i=0 ; i<n ; i++){
+        for (int i = 0; i < n; i++) {
             A[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(A[i]);
         }
     }
 }
