74749262263146f76572e65226c9a221a6d9dbe1
Wonseok Jung <ukksj0621@naver.com>
2023-10-13 23:57:30 +0900

    commit

---
+++ src/Beak_1167.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Beak_1167 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int v = sc.nextInt();
+        ArrayList<Integer>A = new ArrayList<>();
+
+    }
+}
