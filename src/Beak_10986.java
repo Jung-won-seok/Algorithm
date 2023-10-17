e70a6b7fdd75b5844791f97c8f3735d765235ad7
Wonseok Jung <ukksj0621@naver.com>
2023-10-17 23:36:04 +0900

    commit

---
+++ src/Beak_10986.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Beak_10986 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int A[] = new int[N];
+        int M = sc.nextInt();
+        for(int i=0 ; i<N ; i++){
+            A[i] = sc.nextInt();
+        }
+        int cnt = 0;
+        for(int i=0 ; i<N ; i++){
+            int sum = 0;
+            for(int j=i ; j<N ; j++){
+                sum = sum + j;
+                if(sum % M == 0){
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
