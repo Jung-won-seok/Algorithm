3ebd2f1a289f0a6bcdd5f09da654b5fcacef2383
Wonseok Jung <ukksj0621@naver.com>
2023-11-09 23:57:56 +0900

    commit

--- src/practice.java
+++ src/practice.java
@@ -1,44 +1,47 @@
+import java.io.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 public class practice {
-    public static void main(String[] args) {
-        int n = 3;
-        int[] arr = {1, 2, 3};
-        int[] output = new int[n];
-        boolean[] visited = new boolean[n];
-
-        System.out.println();
-        permutation(arr, 0, n, 3);
-    }
-
-    // 사전순으로 순열 구하기
-    // 사용 예시: perm(arr, output, visited, 0, n, 3);
-
-
-    // 순열 구하기
-    // 사용 예시: permutation(arr, 0, n, 4);
-    static void permutation(int[] arr, int depth, int n, int r) {
-        if (depth == r) {
-            print(arr, r);
-            return;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        PriorityQueue<Integer> plus_q = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minus_q = new PriorityQueue<>();
+        int N = sc.nextInt();
+        for(int i=0 ; i<N ; i++){
+            int A = sc.nextInt();
+            if(A > 0){
+                plus_q.add(A);
+            }else{
+                minus_q.add(A);
+            }
         }
-
-        for (int i = depth; i < n; i++) {
-            swap(arr, depth, i);
-            permutation(arr, depth + 1, n, r);
-            swap(arr, depth, i);
+        int sum = 0;
+        while(plus_q.size() > 1){
+            int first = plus_q.poll();
+            int second = plus_q.poll();
+            if(first == 1 && second == 1){
+                sum = sum + first + second;
+            }else {
+                sum = sum + (first * second);
+            }
         }
-    }
-
-    static void swap(int[] arr, int depth, int i) {
-        int temp = arr[depth];
-        arr[depth] = arr[i];
-        arr[i] = temp;
-    }
-
-    // 배열 출력
-    static void print(int[] arr, int r) {
-        for (int i = 0; i < r; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println();
+        if(!plus_q.isEmpty()){
+            sum = sum + plus_q.poll();
+        }
+        while(minus_q.size() > 1){
+            int first = minus_q.poll();
+            int second = minus_q.poll();
+            sum = sum + (first*second);
+        }
+        if(!minus_q.isEmpty()){
+            sum = sum + minus_q.poll();
+        }
+        System.out.println(sum);
     }
 }
+
+
 
